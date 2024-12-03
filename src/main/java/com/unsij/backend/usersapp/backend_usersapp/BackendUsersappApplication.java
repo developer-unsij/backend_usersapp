9049b7118a30c62a887ecce42155dc094e530bbc
nayeli velasco <nayeserratvl@gmail.com>
2024-12-03 17:53:10 -0600

    Implementación del metodo GET

--- src/main/java/com/unsij/backend/usersapp/backend_usersapp/BackendUsersappApplication.java
+++ src/main/java/com/unsij/backend/usersapp/backend_usersapp/BackendUsersappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendUsersappApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendUsersappApplication.class, args);
 	}
