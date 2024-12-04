0bd5932cf92df390afb147a20c9e33475115453c
yenis <yenisleydimartinez13@gmail.com>
2024-12-05 04:06:28 +1300

    agrgacion del crossOrigin

--- src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
+++ src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
@@ -9,14 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.unsij.backend.usersapp.backend_usersapp.models.entities.User;
 import com.unsij.backend.usersapp.backend_usersapp.services.UserService;
@@ -25,6 +18,8 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(originPatterns = "*")
+
 public class UserController {
 
     @Autowired
