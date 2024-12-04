72a7853319e62aa3832e9cb47e620080cfe0952d
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2024-12-04 09:06:22 -0600

    Se agrego la configuracion del CrossOrigin

--- src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
+++ src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,8 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+// @CrossOrigin(originPatterns = "http://localhost:5173")
+@CrossOrigin(originPatterns = "*")
 public class UserController {
 
     @Autowired
