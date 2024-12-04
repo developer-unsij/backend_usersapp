b0d4e50d1df24807eaf8465f320a099485a11ad0
LuisPerezcx <luisperez90377@gmail.com>
2024-12-04 09:07:12 -0600

    crossOrigin en userController

--- src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
+++ src/main/java/com/unsij/backend/usersapp/backend_usersapp/controllers/UserController.java
@@ -9,20 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.unsij.backend.usersapp.backend_usersapp.models.entities.User;
 import com.unsij.backend.usersapp.backend_usersapp.services.UserService;
 
 import jakarta.validation.Valid;
 
+//@CrossOrigin(origins = "http://localhost:5173") //
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
