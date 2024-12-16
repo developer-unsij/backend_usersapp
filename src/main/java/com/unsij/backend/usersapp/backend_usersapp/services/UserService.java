d718941d0d26671374a36d42246cb0c7f9028485
nayeli velasco <nayeserratvl@gmail.com>
2024-12-16 17:58:43 -0600

    se hicieron modificaciones en los codigos, se creo la clase USERREQUEST para mejorar la implementacionde los mensajes de errrores

--- src/main/java/com/unsij/backend/usersapp/backend_usersapp/services/UserService.java
+++ src/main/java/com/unsij/backend/usersapp/backend_usersapp/services/UserService.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.unsij.backend.usersapp.backend_usersapp.models.entities.User;
+import com.unsij.backend.usersapp.backend_usersapp.models.entities.UserRequest;
 
 public interface UserService {
     List<User> findAll();
     Optional<User> findById(Long id);
     User save(User user);
-    Optional<User> update(User user, Long id);
+    Optional<User> update(UserRequest userRequest, Long id);
     void remove(Long id);  
 }
